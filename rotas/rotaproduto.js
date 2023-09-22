9816ac91aad073c3dfa2176631251df0434225c0
Leo Mucci <leocapramucci@gmail.com>
2023-09-22 18:25:46 -0300

    first commit

---
+++ rotas/rotaproduto.js
@@ -0,0 +1,60 @@
+const roteador = require("express").Router();
+const { Model } = require("sequelize")
+const produtos = require("../banco_dados/TabelaProdutos")
+
+const instancia = require("../banco_dados/ModeloTabelaProdutos")
+instancia.sync().then(() => console.log('Tabela criada com sucesso'))
+
+
+roteador.get("/", async(requisicao, resposta) => {
+    resposta.send(
+
+        JSON.stringify(await produtos.listar())
+    )
+});
+
+roteador.post("/", async(requisicao, resposta) => {
+    const {nome, descricao, foto, preco, categoria} = requisicao.body;
+
+    const produto = await produtos.create({nome, descricao, foto, preco, categoria}); 
+    resposta.send(
+
+        JSON.stringify(produto)
+    )
+});
+
+roteador.get("/", async(requisicao, resposta) => {
+    const {id} = requisicao.params;
+    const produto = await produtos.localizar(id);
+    resposta.send(
+
+        JSON.stringify(produto)
+   )
+
+});
+
+
+roteador.put("/:id", async(requisicao, resposta) => {
+    const {id} = requisicao.params;
+    const {nome, descricao, foto, preco, categoria} = requisicao.body;
+
+    const produto = await produtos.atualizar(id, nome, descricao, foto, preco, categoria)
+    resposta.send(
+
+        JSON.stringify(produto)
+   )
+
+});
+
+roteador.delete("/:id", async(requisicao, resposta) => {
+    const {id} = requisicao.params;
+
+    await produtos.remover(id)
+    resposta.send(
+
+        JSON.stringify({mensagem: "Produto removido"})
+   )
+
+});
+
+module.exports = roteador;
